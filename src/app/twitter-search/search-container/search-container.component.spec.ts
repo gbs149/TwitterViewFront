aa866e3bf26c93f21ef961ea4ef8cde24b21a6c9
gabriel <gbs149@outlook.com>
2020-02-23 21:38:12 -0300

    Remove karma/protractor/jasmine

--- src/app/twitter-search/search-container/search-container.component.spec.ts
+++ src/app/twitter-search/search-container/search-container.component.spec.ts
@@ -1,3 +1,4 @@
+import { TwitterSearchService } from './../services/twitter-search.service';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchContainerComponent } from './search-container.component';
@@ -6,10 +7,14 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 describe('SearchContainerComponent', () => {
   let component: SearchContainerComponent;
   let fixture: ComponentFixture<SearchContainerComponent>;
+  let twitterSearch: TwitterSearchService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [SearchContainerComponent],
+      providers: [
+        { provide: TwitterSearchService, useValue: { search: jest.fn() } }
+      ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
   }));
@@ -17,6 +22,7 @@ describe('SearchContainerComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchContainerComponent);
     component = fixture.componentInstance;
+    twitterSearch = TestBed.inject(TwitterSearchService);
     fixture.detectChanges();
   });
 
